6150bc808ae507334c61721e8fdffef4f3b2350d
Caio Tito <caio_tito@hotmail.com>
2023-08-03 15:17:12 -0300

    feat: added binary search challenge

---
+++ Binary Search/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Solution
+{
+    public int Search(int[] nums, int alvo)
+    {
+        int minimo = 0;
+        int maximo = nums.Count() - 1;
+
+        while (minimo <= maximo)
+        {
+            int metade = (minimo + maximo) / 2;
+            int palpite = nums[metade];
+
+            if (palpite == alvo)
+                return metade;
+            if(palpite > alvo)
+                maximo = metade - 1;
+            else
+                minimo = metade + 1;
+        }
+        return -1;
+    }
+}
